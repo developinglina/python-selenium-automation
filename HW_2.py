2e13c5ee2f3b04871f0a8c032657251cb811efc7
Khalina Boyce <96637309+Khalinadenae@users.noreply.github.com>
2025-05-24 21:48:00 -0400

    target sign in test case

---
+++ HW_2.py
@@ -0,0 +1,54 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from time import sleep
+
+# get the path to the ChromeDriver executable
+driver_path = ChromeDriverManager().install()
+
+# create a new Chrome browser instance
+service = Service(driver_path)
+driver = webdriver.Chrome(service=service)
+driver.maximize_window()
+
+# open the url
+driver.get('https://www.target.com/')
+
+sleep(15)
+
+# Test Case: Users can navigate to SignIn page
+
+# Open https://www.target.com/
+driver.get('https://www.target.com/')
+
+# Click Account button
+# id="account-sign-in"
+driver.find_element(By.ID, 'account-sign-in').click()
+sleep(5)
+
+# Click SignIn btn from side navigation
+# data-test="accountNav-signIn"
+driver.find_element(By.XPATH, "//button[@data-test='accountNav-signIn']").click()
+sleep(15)
+
+# Verify SignIn page opened
+expected_text = "Sign in or create account"
+text = driver.find_element(By.XPATH, '//h1[text()="Sign in or create account"]').text
+sleep(15)
+print(text)
+assert expected_text in text
+
+print('test case passed')
+
+# Practice with locators.
+
+# Amazon logo
+# Email field
+# Continue button
+# Conditions of use link
+# Privacy Notice link
+# Need help link
+# Forgot your password link
+# Other issues with Sign-In link
+# Create your Amazon account button
