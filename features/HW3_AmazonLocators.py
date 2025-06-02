e6bf22f968036ad964206bbd651e20faaa9e5f0c
Khalina Boyce <96637309+Khalinadenae@users.noreply.github.com>
2025-06-02 16:17:09 -0400

    amazon locators

---
+++ features/HW3_AmazonLocators.py
@@ -0,0 +1,19 @@
+"""
+AMAZON_LOGO = (By.CLASS_NAME, "a-icon-logo")
+
+Name input field = (By.ID, "ap_customer_name")
+
+EMAIL_INPUT = (By.ID, "ap_email")
+
+PASSWORD_INPUT = (By.ID, "ap_password")
+
+PASSWORD_CHECK_INPUT = (By.ID, "ap_password_check")
+
+CONTINUE_BUTTON = (By.ID, "continue")
+
+CONDITIONS_OF_USE_LINK = (By.LINK_TEXT, "Conditions of Use")
+
+PRIVACY_NOTICE_LINK = (By.LINK_TEXT, "Privacy Notice")
+
+sign in link = (By.LINK_TEXT, "Sign in")
+"""
